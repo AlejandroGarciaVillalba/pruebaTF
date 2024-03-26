26ff8674a2f9c7e1387b2b379cafdfdb58d4d06d
AlejandroGarciaVillalba <alxgarciv@gmail.com>
2024-03-26 08:37:40 +0100

    Cambiado parametro de datasets

--- Codigo/composer/raw-bronze.py
+++ Codigo/composer/raw-bronze.py
@@ -1,8 +1,13 @@
+from google.cloud import storage
 import airflow
 from datetime import datetime
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
 from airflow.operators.python_operator import PythonOperator
 import pandas as pd
+from airflow.operators.dummy_operator import DummyOperator
+from io import BytesIO
+from io import StringIO
+import io
 
 DAG_ID = "raw-bronze"
 
@@ -12,26 +17,72 @@ OBJECT_SRC = "Clientes 1.csv"
 COL_NAME = "FECHA_NACIMIENTO"
 OBJECT_DST = "Clientes 1_Clean.csv"
 
-def  delete_null(OBJECT_SRC, COL_NAME,OBJECT_DST):
-    df = pd.read_csv(OBJECT_SRC)
+def  delete_null():
+    client = storage.Client()
+
+    bucket = client.bucket(BUCKET_NAME_SRC)
+
+    # blob = bucket.blob(OBJECT_SRC)
+    
+    # contenido_bytes = blob.download_as_bytes()
+
+    #df = pd.read_csv(BytesIO(contenido_bytes))
+
+    urls_list = bucket.blob(OBJECT_SRC).download_as_text()
+    csv_io = io.StringIO(urls_list)
+    df = pd.read_csv(csv_io)
+
+    print(df.head(5))
     df = df.dropna (subset=[COL_NAME])
-    df.to_csv(OBJECT_DST, index=False)
+    return df
+
+def insert_bronze_function(df):
+
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+
+    # Convertir el contenido CSV en bytes
+    contenido_bytes = csv_buffer.getvalue().encode()
+
+    # Inicializar cliente de Google Cloud Storage
+    client = storage.Client()
+
+    # Obtener el bucket
+    bucket = client.bucket(BUCKET_NAME_DST)
+
+    # Crear un nuevo blob (archivo) en el bucket
+    blob = bucket.blob(OBJECT_DST)
+
+    # Subir el contenido CSV al blob
+    blob.upload_from_string(contenido_bytes)
 
 with airflow.DAG(
     DAG_ID,
     schedule="0 0 1 * *",
     start_date=datetime(2024, 3, 25),
 ) as dag:
-     delete_null_task = PythonOperator(
-          task_id="limpiar_archivo",
-          python_callable=delete_null(OBJECT_SRC,COL_NAME,OBJECT_DST),
-     )
-
-     copy_single_file = GCSToGCSOperator(
-        task_id="copy_single_gcs_file",
-        source_bucket=BUCKET_NAME_SRC,
-        source_object=[OBJECT_SRC],
-        destination_bucket=BUCKET_NAME_DST,  # If not supplied the source_bucket value will be used
-        destination_object=[OBJECT_DST],  # If not supplied the source_object value will be used
+    delete_null_task = PythonOperator(
+        task_id="delete_null_task",
+        python_callable=delete_null(),
     )
-     delete_null_task >> copy_single_file
+
+    insert_bronze = PythonOperator(
+        task_id="insert_bronze",
+        python_callable=insert_bronze_function(),
+        op_kwargs = {'df': delete_null_task.output}
+    )
+    
+
+    start = DummyOperator(task_id='start')
+
+        
+    end = DummyOperator(task_id='end')
+
+    # Definir la secuencia de tareas
+    start >> delete_null_task >> insert_bronze >> end
+    
+
+
+
+
+
--- IaaC/Modulos/platform/datasets/main.tf
+++ IaaC/Modulos/platform/datasets/main.tf
@@ -8,7 +8,6 @@ resource "google_bigquery_dataset" "dataset_silver" {
   friendly_name               = "silver"
   description                 = "Dataset silver format"
   location                    = var.region
-  default_table_expiration_ms = 3600000
 }
 
 resource "google_bigquery_dataset" "dataset_golden" {
@@ -16,5 +15,4 @@ resource "google_bigquery_dataset" "dataset_golden" {
   friendly_name               = "golden"
   description                 = "Dataset golden format"
   location                    = var.region
-  default_table_expiration_ms = 3600000
 }
